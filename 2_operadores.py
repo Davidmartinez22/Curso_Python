6fe2d77a675e8011293bfc303e88fd2984dca21f
david <1833968@alu.murciaeduca.es>
2024-11-14 16:43:12 +0100

    creacion de operadores

---
+++ 2_operadores.py
@@ -0,0 +1,45 @@
+###Operadores Aritmeticos###
+
+# Operaciones de Enteros
+# Suma de dos numeros
+
+numero1 = 2
+numero2 = 4
+print("Suma de dos numeros")
+print(numero1+numero2)
+
+#resta
+print(numero1-numero2)
+
+#Modulo
+print(numero1%numero2)
+
+#Division Entera sin Decimales
+print(numero1//numero2)
+
+#Exponente
+print(numero1**numero2)
+
+#Combinar operaciones
+print(2**3-7/1//4)
+
+#Operaciones con texto
+print("Hola" + "Jose")
+
+#print("Hola" + 5) Tira error porque 5 es numero y hola es string
+print("Hola" + str(5))
+
+print("Hola" * (3 ** 5))
+
+my_float= 2.5 * 2
+print("hola" * my_float)
+
+print(type(my_float))
+
+## Operadores Comparativos##
+
+print(3 > 4)
+print(3 < 4)
+
+print(numero1 != numero2) #Compara si 3 es diferente de 4. Imprime: True
+
